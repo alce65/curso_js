a7781a29e78d0e4163d14ca73bbff40aae88d476
alce65 <alce65@hotmail.es>
2019-11-13 11:24:34 +0100

    Inicio, version final

---
+++ web/01_hola_mundo/app.js
@@ -0,0 +1,33 @@
+
+// Con import usamos módulos de ES6
+
+import { mostrar} from './utils.js'
+
+const main = () => {
+    let msg = 'Hola Mundo'
+    mostrar()
+    mostrar(msg)    
+
+
+}
+
+main()
+
+
+
+function onButtonClick() {
+    console.log('Pulsado el boton')
+}
+
+'DOMContentLoaded'
+'click'
+'dblclick'
+'inputs'
+
+
+// Funcion anónima autoinvicada 
+/* (() => {
+    let msg = 'Hola Mundo'
+    mostrar()
+    mostrar(msg)    
+})() */
---
+++ web/01_hola_mundo/old.js
@@ -0,0 +1,8 @@
+/* function onButtonClick() {
+    console.log('Pulsado el boton')
+} */
+
+const onButtonClick = () => { 
+    console.log('Pulsado el boton')
+    // alert('Pulsado el boton')
+}
